a7db10c111d5466d5fdc2a21d9316d18b05378e3
jihuayu <8042833@qq.com>
2019-10-29 11:05:36 +0800

    修改项目目的

--- app/model/article.js
+++ app/model/article.js
@@ -1,6 +1,7 @@
 module.exports = app => {
     const mongoose = app.mongoose;
     const schema = new mongoose.Schema({
+        baseUrl: {type: String, required: true},
         blogUrl: {type: String, required: true},
         readNum:  {type: Number, required: true,default:0},
         readTime:  {type: Number, required: true,default: 0},
--- app/router.js
+++ app/router.js
@@ -1,16 +1,20 @@
-'use strict'
-/**
- * @param {Egg.Application} app - egg application
- */
+'use strict';
+
 module.exports = app => {
-  const { router, controller } = app
-  router.post('/api/auth/login', controller.auth.login)
-  router.post('/api/auth/register',controller.auth.register)
-  router.post('/api/auth/logout', controller.auth.logout)
+  const { router, controller } = app;
+  router.post('/api/auth/login', controller.auth.login);
+  router.post('/api/auth/register',controller.auth.register);
+  router.post('/api/auth/logout', controller.auth.logout);
 
-  router.post('/api/user/update', controller.user.update)
+  router.post('/api/user/update', controller.user.update);
 
-  router.post('/api/project/create', controller.user.update)
+  router.post('/api/blog/create', controller.blog.create);
+
+  router.post('/api/article/create', controller.article.create);
+
+  router.post('/api/comment/create', controller.comment.create);
+  router.post('/api/comment/update', controller.comment.update);
+  router.post('/api/comment/delete', controller.comment.delete);
 
   
 
